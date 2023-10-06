e8feae87813d58fc5fbc877a52130e3d4912af01
Lucas Marra Martins <66924008+LucasPyth@users.noreply.github.com>
2023-10-06 18:30:37 -0300

    add delete

--- src/main/java/com/example/budget/controller/ControllerSaving.java
+++ src/main/java/com/example/budget/controller/ControllerSaving.java
@@ -9,8 +9,11 @@ import com.example.budget.saving.SavingRepository;
 import com.example.budget.saving.SavingRequestDTO;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +30,7 @@ public class ControllerSaving {
     
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
-    public void saveSaging(@RequestBody SavingRequestDTO data){
+    public void saveSaving(@RequestBody SavingRequestDTO data){
         
         Saving savingData = new Saving(data);
         repository.save(savingData);
@@ -44,6 +47,18 @@ public class ControllerSaving {
         List<SavingResponseDTO> savingList = repository.findAll().stream().map(SavingResponseDTO::new).toList();
         return savingList;
 
+    }
+    
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity<?> deleteSaving(@PathVariable long id){
+        
+        return repository.findById(id)
+           .map(record -> {
+               repository.deleteById(id);
+               return ResponseEntity.ok().build();
+           }).orElse(ResponseEntity.notFound().build());
     }
     
     
